4c267e3f40af76ad78dc634483ee00ea49c0d6f0
p1v2 <pavliuk96@gmail.com>
2023-12-01 20:19:45 +0200

    move secret key to .env

--- hillel_django/settings.py
+++ hillel_django/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wjieavys4xf#)^" \
-             "9*w$d((ko+_ryn@!^&ka4_mpq3c90$bb0_&h"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
