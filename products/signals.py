2f510279657fb9d148abbe20cb55025722361cc2
Андрей Чумак <andrey.c009893@gmail.com>
2023-12-20 01:52:30 +0200

    celery homework

--- products/signals.py
+++ products/signals.py
@@ -2,9 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from products.models import Order
-from products.tasks import order_created_task
+from products.tasks import gather_and_log_order_statistics
 
 
 @receiver(post_save, sender=Order)
 def order_created(sender, instance, **kwargs):
-    order_created_task.apply_async(args=(instance.id,))
+    gather_and_log_order_statistics.apply_async(args=(instance.id,))
