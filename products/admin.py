3e7a8a3bb716a59af7623125e19fe70aad68534c
p1v2 <pavliuk96@gmail.com>
2024-01-12 19:58:27 +0200

    django admin customizations

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,41 @@
 from django.contrib import admin
+from django.db import models
 
 from products.models import Product, Category, Tag, Order
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Order)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "total_price")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related(
+            "order_products__product")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ("name", "price", "category", "tags")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related("category")
+
+    actions = ["mark_discount"]
+
+    list_display = ("name", "price")
+
+    list_editable = ("price",)
+
+    list_filter = ("category",)
+
+    ordering = ("name",)
+
+    def mark_discount(self, request, queryset):
+        for element in queryset:
+            element.price = element.price * 0.9
+            element.name = f"discounted {element.name}"
+            element.save()
--- products/models/order.py
+++ products/models/order.py
@@ -9,6 +9,18 @@ class Order(models.Model):
 
     @property
     def total_price(self):
+        return self.order_products.aggregate(
+            total_price=models.Sum(
+                models.F("product__price") * models.F("quantity"),
+                output_field=models.FloatField(),
+            )
+        )["total_price"]
+
+    def __str__(self):
+        return f"Order #{self.id} for {self.user.username}"
+
+    @property
+    def total_price(self):
         total = 0
         for order_product in self.order_products.all():
             total += order_product.product.price * order_product.quantity
